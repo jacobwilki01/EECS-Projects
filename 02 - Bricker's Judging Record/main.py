f2cd1fe096a121109a88a8c59cad113a20e43108
jacobwilki01 <jwilkus1@gmail.com>
2022-03-23 14:54:51 -0500

    Judge Records Update

--- 02 - Bricker's Judging Record/main.py
+++ 02 - Bricker's Judging Record/main.py
@@ -1,4 +1,4 @@
-file = open("mytable (19).csv","r")
+file = open("mytable (25).csv","r")
 
 lines = []
 for line in file:
@@ -7,14 +7,31 @@ for line in file:
 
 lines.pop(0)
 
-
-count = 0
-
-for item in lines[0]:
-    print(item)
+total = 0
+sat = 0
+aff = 0
+neg = 0
+overall = 0
 
 for line in lines:
-    if len(line) == 9:
-        count += 1
+    line[8] = line[8].strip()
+    if len(line[8]) > 0:
+        items = line[8].split()
+        items[0] = items[0].strip('"')
+        if line[7] != items[0]:
+            sat += 1
+        total += 1
+    if line[7] == "Aff":
+        aff += 1
+    else:
+        neg += 1
+    overall += 1
 
-print(count)
+print(f"Overall: {overall} | Elim Total: {total} | Sat: {sat} | Aff: {aff} | Neg: {neg}")
+if aff > neg:
+    print("This judge votes aff more often. They have voted aff %.2f%% of the time." % (100 * aff/overall))
+    print(f"That comes out to {aff - neg} more aff votes than neg votes.")
+else:
+    print("This judge votes neg more often. They have voted neg %.2f%% of the time." % (100 * neg/overall))
+    print(f"That comes out to {neg - aff} more neg votes than aff votes.")
+print("This judge has sat %.2f%% of the elims they've judged." % (100 * sat/total))
